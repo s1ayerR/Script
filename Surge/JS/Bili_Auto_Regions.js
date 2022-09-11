ba111384145bf7ae2da2dc02ad679fd5f1553cf9
s1ayerR <113167729+s1ayerR@users.noreply.github.com>
2022-09-11 11:38:06 +0800

    Update Bili_Auto_Regions.js

--- Surge/JS/Bili_Auto_Regions.js
+++ Surge/JS/Bili_Auto_Regions.js
@@ -351,6 +351,16 @@ function nobyda() {
 			})
 		}
 	}
+	const Group = $.read('BiliArea_Policy') || '（BiliGroup）'; //Your blibli policy group name.
+	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
+	const TW = $.read('BiliArea_TW') ||  //Your Taiwan sub-policy name.
+	const HK = $.read('BiliArea_HK') ||  //Your HongKong sub-policy name.
+	const DF = $.read('BiliArea_DF') ||  //Sub-policy name used after region is blocked(e.g. url 404)
+	const off = $.read('BiliArea_disabled') ||  //WiFi blacklist(disable region change), separated by commas.
+	const current = await $.getPolicy(Group)
+			})
+		}
+	}
 	return {
 		getPolicy,
 		setPolicy,
